1d63749ab2559a0c934c9e991d1653922a8e4b39
Jonathan Byte <jonathan.cruzpalacios@outlok.com>
2022-03-21 06:53:49 -0700

    Swgger

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/mutant', to: 'mutants#index'
   get '/stats', to: 'mutants#stats'
