cb9a081a475b1f1e785905fa6e53230030778a05
Jonathan Byte <jonathan.cruzpalacios@outlok.com>
2022-03-21 17:22:30 -0700

    status change

--- app/controllers/mutants_controller.rb
+++ app/controllers/mutants_controller.rb
@@ -1,20 +1,10 @@
 class MutantsController < ApplicationController
   
-  rescue_from Exception do |e|
-    render json: {error: e.message}, status: :internal_error
-  end
-
-  rescue_from ActiveRecord::RecordNotFound do |e|
-    render json: {error: e.message}, status: :not_found
-  end
-
-  rescue_from ActiveRecord::RecordInvalid do |e|
-    render json: {error: e.message}, status: :unprocessable_entity
-  end
+  
 
   # POST /mutant
   def index
     is_mutant = MutantService.run params['dna']
-    render json: {is_mutant: is_mutant}, status: is_mutant ? :ok : :not_found
+    render json: {is_mutant: is_mutant}, status: is_mutant ? :ok : :forbidden
   end
 end
